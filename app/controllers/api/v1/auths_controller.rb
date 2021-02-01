058a27f7548a0e6b722015d5d906a16ead431fea
Chris Kay <ckay84@protonmail.com>
2021-02-01 18:59:47 -0500

    Generate JWT on successful login and include in response

--- app/controllers/api/v1/auths_controller.rb
+++ app/controllers/api/v1/auths_controller.rb
@@ -3,7 +3,11 @@ class Api::V1::AuthsController < ApplicationController
     def create
         user = User.find_by(username: params[:user][:username])
         if user && user.authenticate(params[:user][:password])
-            render json: user
+            token = generate_token({ id: user.id })
+            render json: {
+                user: user,
+                jwt: token
+            }, status: :ok
         else
             render json: {
                 error: 'Invalid credentials',
