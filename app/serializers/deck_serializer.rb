68b97c1c0ba162ce9a54b89fd402132615302cd7
Chris Kay <ckay84@protonmail.com>
2021-02-01 14:48:18 -0500

    Add relationships to serializers

--- app/serializers/deck_serializer.rb
+++ app/serializers/deck_serializer.rb
@@ -1,3 +1,5 @@
 class DeckSerializer < ActiveModel::Serializer
   attributes :id, :name
+  belongs_to :user, foreign_key: "user_id"
+  has_many :cards
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :password
+  has_many :decks, foreign_key: "reference_id"
 end
