6afb068cbd63b6c28093c941c1884a185b0b1fe7
Chris Kay <ckay84@protonmail.com>
2021-02-01 18:57:50 -0500

    Add helper methods to generate and decode JWTs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+
+    def generate_token(payload)
+        token = JWT.encode(payload, 'token')
+    end
+
+    def decode_token_and_get_user_id
+        JWT.decode(request.headers['Authorization'], 'token')[0]['id']
+    end
+
 end
