e99048b6dfb98ae3ea6688d0216a101edfef1fb3
Chris Kay <ckay84@protonmail.com>
2021-02-02 15:26:40 -0500

    Add create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,6 +10,19 @@ class Api::V1::UsersController < ApplicationController
         render json: user, status: :ok
     end
 
+    def create 
+        user = User.new(user_params)
+        if user.save
+            token = generate_token({ id: user.id })
+            render json: {
+                user: user,
+                jwt: token
+            }, status: :created
+        else
+            render json: user.errors, status: :unprocessable_entity
+        end
+    end
+
     private
 
     def user_params
