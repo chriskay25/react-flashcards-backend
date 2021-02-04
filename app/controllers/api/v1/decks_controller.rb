7b2b8e59ee9c32fc5a634b0391158f9582ed1163
Chris Kay <ckay84@protonmail.com>
2021-02-03 23:55:58 -0500

    Add a show action

--- app/controllers/api/v1/decks_controller.rb
+++ app/controllers/api/v1/decks_controller.rb
@@ -5,6 +5,11 @@ class Api::V1::DecksController < ApplicationController
         render json: decks
     end
 
+    def show
+        deck = Deck.find_by(id: params[:id])
+        render json: deck
+    end
+
     private
 
     def deck_params
