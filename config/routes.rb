23eb769715ec0420dfe6045645a1e278205acf80
Chris Kay <ckay84@protonmail.com>
2021-02-03 23:58:43 -0500

    Get route for user auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/login', to: 'auths#create'
+      get '/auth', to: 'auths#get_current_user'
       resources :users
       resources :decks
       resources :cards
