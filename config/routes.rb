c5c16127474a4c7133c3c37da51f5e1e3ad98492
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-02-09 00:00:49 -0500

    Add signup route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/login', to: 'auths#create'
+      post '/signup', to: 'users#create'
       get '/auth', to: 'auths#get_current_user'
       resources :users
       resources :decks
