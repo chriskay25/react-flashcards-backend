ec3e3c67f9aee80cedf23631047fc4904b74b3af
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-08-25 23:59:31 -0400

    Add create and update actions

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -5,9 +5,32 @@ class Api::V1::CardsController < ApplicationController
         render json: cards
     end
 
+    def create
+        if logged_in?
+            deck = logged_in_user.decks.find_by(id: params[:deck_id])
+            card = deck.cards.build(card_params)
+            if card.save
+                render json: ActiveModelSerializers::SerializableResource.new(card).to_json, status: :created
+            else
+                render json: card.errors, status: :unprocessable_entity
+            end
+        else
+            render json: {error: "You do not appear to be logged in."}
+        end
+    end
+
+    def update
+        if logged_in?
+            card = Card.find_by(id: params[:id])
+            card.update(card_params)
+            render json: ActiveModelSerializers::SerializableResource.new(card).to_json, status: :ok
+            # binding.pry
+        end
+    end
+
     private
 
     def card_params
-        params.require(:card).permit(:question, :answer, :hint, :collection_id)
+        params.require(:card).permit(:question, :answer, :hint, :deck_id)
     end
 end
